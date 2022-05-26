96518c72e6b0e2b37600073a7342fe127ad2ea8b
jessibera <jessibera.98@gmail.com>
2022-05-26 19:14:26 +0200

    Actualización: Enlaces html y muestra a través de controladores

--- src/main/java/com/example/trivial_tfg/controller/UsuarioController.java
+++ src/main/java/com/example/trivial_tfg/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.example.trivial_tfg.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.trivial_tfg.services.UsuarioService;
 
@@ -10,5 +11,9 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
-	
+	@GetMapping("/perfil")
+	public String getPerfil() {
+
+		return "html/perfil";
+	}
 }
