03225b372024185334bc29bd60cf9b2103c0ad58
jessibera <jessibera.98@gmail.com>
2022-06-11 02:29:52 +0200

    Diseño del index, actualizado.

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,3 +2,33 @@
 ////////////////////////////IMPORT JS/////////////////////////
 //document.write('<script src="../../static/js/ranking.js"></script>');
 //document.write('<script src="../../static/js/perfil.js"></script>');
+
+//////////////////////////////////////////////////////////////
+/////////////////////CAMBIAR ICONO IDIOMAS////////////////////
+function quitar_en() {
+	var eng = document.getElementById("english");
+	eng.setAttribute("style", "display:none");
+
+	return eng;
+}
+
+function poner_es() {
+	var esp = document.getElementById("espana");
+	esp.removeAttribute("style");
+
+	return esp;
+}
+
+function quitar_es() {
+	var esp = document.getElementById("espana");
+	esp.setAttribute("style", "display:none");
+
+	return esp;
+}
+
+function poner_en() {
+	var eng = document.getElementById("english");
+	eng.removeAttribute("style");
+
+	return eng;
+}
